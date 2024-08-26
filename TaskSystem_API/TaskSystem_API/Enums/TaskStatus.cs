609c65c3494221dc1174bb64c70553902ea92984
Talita Dias <talita.dias@itriad.org.br>
2024-08-26 11:23:34 -0400

    feat(enums): implement enum to TaskStatus

---
+++ TaskSystem_API/TaskSystem_API/Enums/TaskStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace TaskSystem_API.Enums
+{
+    public enum TaskStatus
+    {
+        [Description("A fazer")]
+        AFazer = 1,
+        [Description("Em andamento")]
+        EmAndamento = 2,
+        [Description("Concluído")]
+        Concluido = 3
+    }
+}
