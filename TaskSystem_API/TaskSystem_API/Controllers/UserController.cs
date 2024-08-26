94f74670af9920572b628cccdbca3e10f656bf27
Talita Dias <talita.dias@itriad.org.br>
2024-08-26 09:43:51 -0400

    feat(controllers): implement UserController

---
+++ TaskSystem_API/TaskSystem_API/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskSystem_API.Models;
+
+namespace TaskSystem_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UserModel>> GetAllUsers()
+        {
+            return Ok();
+        }
+    }
+}
