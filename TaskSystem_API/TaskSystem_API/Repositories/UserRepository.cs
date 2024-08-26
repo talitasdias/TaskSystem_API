584fb41066a2a6d6a5ab355e49864d519529388e
Talita Dias <talita.dias@itriad.org.br>
2024-08-26 10:33:32 -0400

    feat(repositories): implement interface methods in UserRepository

---
+++ TaskSystem_API/TaskSystem_API/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using TaskSystem_API.Models;
+using TaskSystem_API.Repositories.Interfaces;
+
+namespace TaskSystem_API.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<UserModel> Add(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserModel>> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserModel> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserModel> Uptade(UserModel user, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
