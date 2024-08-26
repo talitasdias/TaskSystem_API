b8f126d142112079148a5687d6560b39d029c4b3
Talita Dias <talita.dias@itriad.org.br>
2024-08-26 13:15:14 -0400

    feat(repositories/interfaces): implement ITaskRepository interface

---
+++ TaskSystem_API/TaskSystem_API/Repositories/Interfaces/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskSystem_API.Models;
+
+namespace TaskSystem_API.Repositories.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<List<TaskModel>> GetAllTasks();
+        Task<TaskModel> GetById(int id);
+        Task<TaskModel> Add(TaskModel tarefa);
+        Task<TaskModel> Update(TaskModel tarefa, int id);
+        Task<bool> Delete(int id);
+    }
+}
