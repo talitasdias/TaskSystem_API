233e0c540534978f88392f028f6b24105798326c
Talita Dias <talita.dias@itriad.org.br>
2024-08-26 10:23:05 -0400

    feat(repositories/interfaces): implement IUserRepository

---
+++ TaskSystem_API/TaskSystem_API/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskSystem_API.Models;
+
+namespace TaskSystem_API.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<UserModel>> GetAllUsers();
+        Task<UserModel> GetById(int id);
+        Task<UserModel> Add(UserModel user);
+        Task<UserModel> Uptade(UserModel user, int id);
+        Task<bool> Delete(int id);
+    }
+}
