b5496ad54826e69807dd596da81bcd18f73baa31
Talita Dias <talita.dias@itriad.org.br>
2024-08-26 09:58:44 -0400

    feat(data): implement db context

---
+++ TaskSystem_API/TaskSystem_API/Data/TaskSystemDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskSystem_API.Models;
+
+namespace TaskSystem_API.Data
+{
+    public class TaskSystemDBContext : DbContext
+    {
+        public TaskSystemDBContext(DbContextOptions<TaskSystemDBContext> options)
+            : base(options)
+        {            
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<TaskModel> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
