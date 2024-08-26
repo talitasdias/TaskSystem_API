e0235b64f472a5dc56c77c7dfe78287a69ef8cc3
Talita Dias <talita.dias@itriad.org.br>
2024-08-26 11:38:06 -0400

    feat(data/map): implement entity mapping for TaskMap

---
+++ TaskSystem_API/TaskSystem_API/Data/Map/TaskMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskSystem_API.Models;
+
+namespace TaskSystem_API.Data.Map
+{
+    public class TaskMap : IEntityTypeConfiguration<UserModel>
+    {
+        public void Configure(EntityTypeBuilder<UserModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.Description).HasMaxLength(1000);
+            builder.Property(x => x.Status).IsRequired();
+        }
+    }
+}
