061ebe9a882958201c648d8dd7be353549b447d5
Talita Dias <talita.dias@itriad.org.br>
2024-08-26 13:43:36 -0400

    feat(controllers): create TaskController

---
+++ TaskSystem_API/TaskSystem_API/Controllers/TaskController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskSystem_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+    }
+}
