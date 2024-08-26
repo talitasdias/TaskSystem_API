7eb0ded5df747c00275cbacde012364db79de8ae
Talita Dias <talita.dias@itriad.org.br>
2024-08-26 13:18:38 -0400

    feat(repositories): create TaskRepository class

---
+++ TaskSystem_API/TaskSystem_API/Repositories/TaskRepository.cs
@@ -0,0 +1,33 @@
+﻿using TaskSystem_API.Models;
+using TaskSystem_API.Repositories.Interfaces;
+
+namespace TaskSystem_API.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        public Task<TaskModel> Add(TaskModel tarefa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TaskModel>> GetAllTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskModel> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskModel> Update(TaskModel tarefa, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
