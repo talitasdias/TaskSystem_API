579c03c46f51d3da31c8439f23445a8c55a5f910
Talita Dias <talita.dias@itriad.org.br>
2024-08-26 13:40:40 -0400

    feat(program): register ITaskRepository

--- TaskSystem_API/TaskSystem_API/Program.cs
+++ TaskSystem_API/TaskSystem_API/Program.cs
@@ -25,6 +25,7 @@ namespace TaskSystem_API
                 );
 
             builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 
             var app = builder.Build();
 
