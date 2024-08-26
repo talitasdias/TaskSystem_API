741e6cd753f5525348d24a4e81f3439cf5e4cd8a
Talita Dias <talita.dias@itriad.org.br>
2024-08-26 11:26:33 -0400

    refactor(models): adjust the type of Status

--- TaskSystem_API/TaskSystem_API/Models/TaskModel.cs
+++ TaskSystem_API/TaskSystem_API/Models/TaskModel.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Descricao { get; set; }
-        public int Status { get; set; }
+        public TaskStatus Status { get; set; }
     }
 }
