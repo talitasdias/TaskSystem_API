00926b45cafbf507901c1e1887c4c518ae7a5434
Talita Dias <talita.dias@itriad.org.br>
2024-08-28 11:20:25 -0400

    feat(services): implement interface in the UserService class

---
+++ TaskSystem_API/TaskSystem_API/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using TaskSystem_API.Models;
+using TaskSystem_API.Services.Interfaces;
+
+namespace TaskSystem_API.Services
+{
+    public class UserService : IUserService
+    {
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserModel>> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserModel> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserModel> Register(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserModel> Update(UserModel userModel, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
