aa2cc00ff5ab11d2df47602a97839622857df55d
Talita Dias <talita.dias@itriad.org.br>
2024-08-28 11:18:21 -0400

    feat(services/interfaces): create ITaskService interface

---
+++ TaskSystem_API/TaskSystem_API/Services/Interfaces/ITaskService.cs
@@ -0,0 +1,13 @@
+﻿using TaskSystem_API.Models;
+
+namespace TaskSystem_API.Services.Interfaces
+{
+    public interface ITaskService
+    {
+        Task<List<TaskModel>> ListAll();
+        Task<TaskModel> GetById(int id);
+        Task<TaskModel> Register(TaskModel taskModel);
+        Task<TaskModel> Update(TaskModel newTask, int id);
+        Task<bool> Delete(int id);
+    }
+}
