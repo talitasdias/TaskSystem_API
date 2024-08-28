6f96a8bba6d2b5213ecd484414f31fda6388ea37
Talita Dias <talita.dias@itriad.org.br>
2024-08-28 13:44:35 -0400

    refactor(controllers): replace ITaskRepository with ITaskService in TaskController

--- TaskSystem_API/TaskSystem_API/Controllers/TaskController.cs
+++ TaskSystem_API/TaskSystem_API/Controllers/TaskController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TaskSystem_API.Models;
-using TaskSystem_API.Repositories.Interfaces;
+using TaskSystem_API.Services.Interfaces;
 
 namespace TaskSystem_API.Controllers
 {
@@ -9,30 +9,30 @@ namespace TaskSystem_API.Controllers
     [ApiController]
     public class TaskController : ControllerBase
     {
-        private readonly ITaskRepository _taskRepository;
-        public TaskController(ITaskRepository taskRepository)
+        private readonly ITaskService _taskService;
+        public TaskController(ITaskService taskService)
         {
-            _taskRepository = taskRepository;
+            _taskService = taskService;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<TaskModel>>> listAll()
+        public async Task<ActionResult<List<TaskModel>>> GetAllTasks()
         {
-            List<TaskModel> tasks = await _taskRepository.GetAllTasks();
+            List<TaskModel> tasks = await _taskService.ListAll();
             return Ok(tasks);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<TaskModel>> GetById(int id)
         {
-            TaskModel task = await _taskRepository.GetById(id);
+            TaskModel task = await _taskService.GetById(id);
             return Ok(task);
         }
 
         [HttpPost]
-        public async Task<ActionResult<TaskModel>> Register([FromBody] TaskModel taskModel)
+        public async Task<ActionResult<TaskModel>> Add([FromBody] TaskModel taskModel)
         {
-            TaskModel task = await _taskRepository.Add(taskModel);
+            TaskModel task = await _taskService.Register(taskModel);
 
             return Ok(task);
         }
@@ -41,7 +41,7 @@ namespace TaskSystem_API.Controllers
         public async Task<ActionResult<TaskModel>> Update([FromBody] TaskModel taskModel, int id)
         {
             taskModel.Id = id;
-            TaskModel task = await _taskRepository.Update(taskModel, id);
+            TaskModel task = await _taskService.Update(taskModel, id);
 
             return Ok(task);
         }
@@ -49,7 +49,7 @@ namespace TaskSystem_API.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult<TaskModel>> Delete(int id)
         {
-            bool deleted = await _taskRepository.Delete(id);
+            bool deleted = await _taskService.Delete(id);
 
             return Ok(deleted);
         }
