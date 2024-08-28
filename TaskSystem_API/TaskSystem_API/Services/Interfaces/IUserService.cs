e91a245eee9c9ced8b1671b681b1307c8cc6bef3
Talita Dias <talita.dias@itriad.org.br>
2024-08-28 11:15:58 -0400

    feat(services/interfaces): create IUserService interface

---
+++ TaskSystem_API/TaskSystem_API/Services/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using TaskSystem_API.Models;
+
+namespace TaskSystem_API.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<List<UserModel>> GetAllUsers();
+        Task<UserModel> GetById(int id);
+        Task<UserModel> Register(UserModel userModel);
+        Task<UserModel> Update(UserModel userModel, int id);
+        Task<bool> Delete(int id);
+    }
+}
